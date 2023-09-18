18c5c4ddf99e579f0b9ca9f823eff4c646abd797
Abi <abichitrakar2921@gmail.com>
2023-09-18 16:30:53 -0400

    MAPD-712-LAB-2-Task-3

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {View, Text, TextInput,  Button,StyleSheet,  Switch, } from "react-native";
 
-export default function App() {
+export default function HelloWorldApp() {
+  const [value, onChangeText] = React.useState("");
+  const [isEnabled, setIsEnabled]= useState(false);
+  const [bgColor, setBgColor] = useState("white");
+  const [color, setColor] = useState("black");
+  // Task 1 and Task 2
+  const toggleMode =()=>{
+    setIsEnabled(prev => !prev)
+    setBgColor(isEnabled ? 'white': 'black')
+    setColor(isEnabled ? 'black': 'white')
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{...styles.container, backgroundColor:bgColor}}>
+      <TextInput
+        style={{...styles.edit, color: color, }}
+      
+        onChangeText={(text) => onChangeText(text)}
+        placeholder="Input your text"
+        placeholderTextColor={color}
+        value={value}
+      />
+      <Text style={{...styles.edit, color: color }} >{value}</Text>
+      <Button
+        style={styles.button}
+        onPress={() => onChangeText("")}
+        title="Clear"
+      />
+      <Switch onValueChange={toggleMode} value={isEnabled}></Switch>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  edit: {
+    padding:30,
+    fontSize: 30,
   },
 });
+
+
